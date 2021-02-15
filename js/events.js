c80ad50992ba5ff3480317a1ff4486eb21a88509
Kyle Samuel <ksamuel92@gmail.com>
2021-02-15 00:20:42 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt () {
   });
 }
 
-function submitIt() {
+function submitIt(event) {
   $('#typing').on('submit', 
   function()  {
     alert('Your form is going to be submitted now.');
